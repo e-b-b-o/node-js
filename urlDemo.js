c7b724cf2f39d91cd3da6a1e98bc08a2e2fa80be
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-07 08:46:46 +0300

    refactor(url): remove unused fileURLToPath import

--- urlDemo.js
+++ urlDemo.js
@@ -1,1 +1,29 @@
 import url from "url";
+
+const urlString = "https://www.google.com/search?q=hello+world";
+
+// url Object
+
+const urlObj = new URL(urlString);
+
+//url format
+
+console.log(url.format(urlObj));
+
+//import.meta.url fileURl
+
+console.log(import.meta.url);
+
+//  fileUrl to path
+
+console.log(url.fileURLToPath(import.meta.url));
+
+// URLSearchParams
+
+const params = new URLSearchParams(urlObj.search);
+
+params.append("limit", "5");
+params.delete("limit");
+console.log(params);
+
+console.log(params.get("q"));
